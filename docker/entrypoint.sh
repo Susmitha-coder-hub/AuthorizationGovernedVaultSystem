b7a474dfb7936c2e0179d6ea622a342c4c1f6c4a
Susmitha <susmithanandigramam.0215@gmail.com>
2025-12-26 20:43:58 +0530

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-npx hardhat compile
+echo "Starting Hardhat local blockchain..."
+npx hardhat node --hostname 0.0.0.0 &
+
+sleep 5
+
+echo "Deploying contracts..."
 npx hardhat run scripts/deploy.js --network localhost
+
+tail -f /dev/null
